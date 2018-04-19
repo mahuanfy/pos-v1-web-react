939f34631e2219dee671e79c74d4b778e30195ae
mahuanfy <mahuanfy@foxmail.com>
2018-04-19 11:56:12 +0800

    完成页面

--- src/Content.js
+++ src/Content.js
@@ -3,7 +3,12 @@ import './App.css';
 import './bootstrap/css/bootstrap.min.css'
 
 class Content extends Component {
+    shopping(barcode){
+        alert(barcode+"加入购物车成功");
+        
+    }
     render() {
+        var count = this.props.datbase.length
         return (
             <div class="row">
 
@@ -14,24 +19,26 @@ class Content extends Component {
                         <h1>欢迎观临线上特训营超市 <small>001分店</small></h1>
                     </div>
                     <div class="row">
-                        <div class="col-sm-6 col-md-4">
-                            <div class="thumbnail">
-                                <img src={require('./img/ITEM000001.jpg')} alt="..." />
-                                <div class="caption">
-                                    <h4>名称：雪碧</h4>
-                                    <p>编号：ITEM000001</p>
-                                    <p>3.00/瓶</p>
-                                    <p>
-                                        <a href="#" class="btn btn-primary" role="button">加入购物车</a>
-                                            <div class="input-group">
+                        {this.props.datbase.map(item => {
+                            return (<div class="col-sm-6 col-md-4">
+                                <div class="thumbnail">
+                                    <img class='img' src={require('./img/'+item.barcode+'.jpg')} alt="..." />
+                                    <div class="caption">
+                                        <h4>名称：{item.name}</h4>
+                                        <p>编号：{item.barcode}</p>
+                                        <p>单价：{item.price}元/{item.unit}</p>
+                                        <p>
+                                        <button type="button" class="btn btn-primary" onClick={this.shopping.bind(this,item.barcode)}>加入购物车</button>
+                                            <div class="input-group unit">
                                                 <div class="input-group-addon">个数：</div>
-                                                <input type="number" class="form-control" id="exampleInputAmount" value='1' />
-                                                <div class="input-group-addon">瓶</div>
-                                        </div>
-                                    </p>
+                                                <input type="number" class="form-control" id="exampleInputAmount" placeholder='1' />
+                                                <div class="input-group-addon">{item.unit}</div>
+                                            </div>
+                                        </p>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
+                            </div>)
+                        })}
                     </div>
                 </div>
                 <div class="col-md-2"></div>
